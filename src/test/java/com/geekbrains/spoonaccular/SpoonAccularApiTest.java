e931dfce35daa08b689b2e71165bf46d4bfd1b6e
KotelnikovNS <91054969+KotelnikovNS@users.noreply.github.com>
2022-04-18 18:49:09 +0300

    Add files via upload

---
+++ src/test/java/com/geekbrains/spoonaccular/SpoonAccularApiTest.java
@@ -0,0 +1,153 @@
+package com.geekbrains.spoonaccular;
+
+import com.geekbrains.BaseTest;
+import com.geekbrains.clients.SpoonacularClient;
+import com.geekbrains.spoonaccular.model.*;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import net.javacrumbs.jsonunit.JsonAssert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
+import static org.hamcrest.Matchers.lessThan;
+
+public class SpoonAccularApiTest extends BaseTest {
+
+    private static final String API_KEY = "0b901035bc9243459ebe3b7969ede958";
+    private static final String BASE_URL = "https://api.spoonacular.com";
+
+    private static SpoonacularClient client;
+
+    @BeforeAll
+    static void beforeAll() {
+
+        RestAssured.baseURI = BASE_URL;
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("apiKey", API_KEY)
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectResponseTime(lessThan(1000L))
+                .build();
+
+        client = new SpoonacularClient();
+    }
+
+    @Test
+    void testGetComplexSearch() throws IOException {
+
+        String actually = given()
+                .queryParam("query", "pasta")
+                .queryParam("cuisine", "italian")
+                .log()
+                .uri()
+                .expect()
+                .log()
+                .body()
+                .when()
+                .get("recipes/complexSearch")
+                .body()
+                .asPrettyString();
+
+        String expected = getResourceAsString("expected.json");
+
+        JsonAssert.assertJsonEquals(
+                expected,
+                actually,
+                JsonAssert.when(IGNORING_ARRAY_ORDER)
+        );
+
+    }
+
+    @Test
+    void testGetComplexSearchPojo() throws IOException {
+
+        RecipesSearchResponse actually = given()
+                .queryParam("query", "pasta")
+                .queryParam("cuisine", "italian")
+                .log()
+                .uri()
+                .expect()
+                .log()
+                .body()
+                .when()
+                .get("recipes/complexSearch")
+                .body()
+                .as(RecipesSearchResponse.class);
+
+        System.out.println(actually);
+
+        for (RecipesSearchResponseItem item : actually.getResults()) {
+            Assertions.assertNotNull(item.getId());
+            Assertions.assertTrue(item.getTitle().toLowerCase(Locale.ROOT).contains("pasta"));
+            Image image = ImageIO.read(new URL(item.getImage()));
+            Assertions.assertNotNull(image);
+        }
+    }
+
+    @Test
+    void testReceiptAutoComplete() throws IOException {
+
+        List<AutoCompleteRecipeItem> actually = given()
+                .queryParam("query", "pizza")
+                .queryParam("number", 10)
+                .log()
+                .uri()
+                .expect()
+                .log()
+                .body()
+                .when()
+                .get("recipes/autocomplete")
+                .body()
+                .as(new TypeRef<>() {});
+
+        String expected = getResourceAsString("autocomplete/expected.json");
+
+        JsonAssert.assertJsonEquals(
+                expected,
+                actually,
+                JsonAssert.when(IGNORING_ARRAY_ORDER)
+        );
+    }
+
+    @Test
+    void testProductSearchGrocery() throws IOException {
+
+        SearchGroceryProductsResponse products = client.findAllProducts(
+                SearchGroceryProductsRequest.builder()
+                        .query("pasta")
+                        .minCalories(10L)
+                        .maxCalories(1000L)
+                        .number(3L)
+                        .build()
+        );
+
+        String expected = getResourceAsString("products.json");
+
+        JsonAssert.assertJsonEquals(
+                expected,
+                products,
+                JsonAssert.when(IGNORING_ARRAY_ORDER)
+        );
+
+
+    }
+
+}
